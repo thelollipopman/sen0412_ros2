c70c004359235a7fadf7ce3b87b6e945a647c698
thelollipopman <leechengyongcephas@gmail.com>
2026-08-26 12:51:09 +0800

    added launch file

---
+++ launch/sen0412.launch.py
@@ -0,0 +1,51 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, OpaqueFunction
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+
+def launch_setup(context):
+    package_share = get_package_share_directory("sen0412_ros2")
+
+    config_file = LaunchConfiguration(
+        "config_file"
+    ).perform(context)
+
+    if not os.path.isabs(config_file):
+        config_file = os.path.join(
+            package_share,
+            "config",
+            config_file
+        )
+
+    node = Node(
+        package="sen0412_ros2",
+        executable="sen0412_node",
+        name="sen04120_node",
+        output="screen",
+        parameters=[config_file],
+    )
+
+    return [node]
+
+
+def generate_launch_description():
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            "config_file",
+            default_value="config.yaml",
+            description=(
+                "Parameter YAML file. Relative paths are "
+                "resolved against the package config directory."
+            ),
+        ),
+
+        OpaqueFunction(
+            function=launch_setup
+        ),
+    ])
